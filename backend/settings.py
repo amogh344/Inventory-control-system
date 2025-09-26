daedea66a796fb1151833163266b3fe48ceb772a
Amogh Brahma R <amoghbrahma@gmail.com>
2025-09-26 23:17:03 +0530

    feat: Prepare project for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # ============================================================================
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=['http://localhost:3000'])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['http://localhost:3000'])
 
 # ============================================================================
 #  APPLICATION DEFINITION
@@ -71,9 +73,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
 
 # ============================================================================
 #  REST FRAMEWORK CONFIGURATION
