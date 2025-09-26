2838dc5f479a8310185c8fa96791a66f0fa30c7a
Amogh Brahma R <amoghbrahma@gmail.com>
2025-09-26 21:23:50 +0530

    modded

--- backend/settings.py
+++ backend/settings.py
@@ -163,6 +163,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')                                                                                                                      
+EMAIL_USE_TLS = True                                                                                                                 
