46436ba1885334bfbfbe992907bac6024ebec43d
Amogh Brahma R <amoghbrahma@gmail.com>
2025-09-27 00:18:35 +0530

    fix: Add makemigrations step to build script

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,7 @@
 # 1. Install all Python packages from your requirements file
 pip3 install -r backend/requirements.txt
 
+python3 manage.py makemigrations
+
 # 2. Run database migrations against your live database
 python3 manage.py migrate
