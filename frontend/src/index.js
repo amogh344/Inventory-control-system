7bb24edc4cb2035dc6f582d04c81a7b16b38e649
Amogh Brahma R <amoghbrahma@gmail.com>
2025-09-26 23:02:42 +0530

    Add bootstrap-icons and react-icons dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './styles/theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { ToastProvider } from './components/ui/ToastProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
